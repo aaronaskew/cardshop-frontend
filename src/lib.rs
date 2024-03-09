44b70c5fe0af06ca00f5e4065401ae411d638c0c
Aaron Askew <aaronaskew@gmail.com>
2024-03-09 09:54:11 -0600

    initial commit created using `cargo leptos new --init --git leptos-rs/start-axum`

--- src/lib.rs
+++ src/lib.rs
@@ -1,36 +1,12 @@
-use leptos::*;
-use leptos_meta::*;
-use leptos_router::*;
+pub mod app;
+pub mod error_template;
+#[cfg(feature = "ssr")]
+pub mod fileserv;
 
-// Modules
-mod components;
-mod pages;
-
-// Top-Level pages
-use crate::pages::home::Home;
-use crate::pages::not_found::NotFound;
-
-/// An app router which renders the homepage and handles 404's
-#[component]
-pub fn App() -> impl IntoView {
-    // Provides context that manages stylesheets, titles, meta tags, etc.
-    provide_meta_context();
-
-    view! {
-        <Html lang="en" dir="ltr" attr:data-theme="light"/>
-
-        // sets the document title
-        <Title text="Welcome to Leptos CSR"/>
-
-        // injects metadata in the <head> of the page
-        <Meta charset="UTF-8"/>
-        <Meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-
-        <Router>
-            <Routes>
-                <Route path="/" view=Home/>
-                <Route path="/*" view=NotFound/>
-            </Routes>
-        </Router>
-    }
+#[cfg(feature = "hydrate")]
+#[wasm_bindgen::prelude::wasm_bindgen]
+pub fn hydrate() {
+    use crate::app::*;
+    console_error_panic_hook::set_once();
+    leptos::mount_to_body(App);
 }
